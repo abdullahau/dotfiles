c467d9bf2af1a963008ff90a7e8e148bae266d7d
Abdullah Mahmood <abdullah.au@outlook.com>
2025-10-25 17:38:14 +0400

    moved Brewfiles to a dedicated packages directory

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -11,13 +11,12 @@ fi
 
 # TODO: Keep an eye out for a different `--no-quarantine` solution.
 # Currently, you can't do `brew bundle --no-quarantine` as an option.
-# export HOMEBREW_CASK_OPTS="--no-quarantine --no-binaries"
+# export HOMEBREW_CASK_OPTS="--no-quarantine"
 # https://github.com/Homebrew/homebrew-bundle/issues/474
 
 # HOMEBREW_CASK_OPTS is exported in `zshenv` with
-# `--no-quarantine` and `--no-binaries` options,
-# which makes them available to Homebrew for the
-# first install (before our `zshrc` is sourced).
+# `--no-quarantine` options, which makes them available 
+# to Homebrew for the first install (before our `zshrc` is sourced).
 
 brew bundle --verbose
 
