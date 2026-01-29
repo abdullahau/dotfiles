0a2bae1ec75ace42b2f6671d06f19954ef93eeb7
Abdullah Mahmood <abdullah.au@outlook.com>
2026-01-29 09:47:39 +0400

    updates

--- setup_ubuntu.zsh
+++ setup_ubuntu.zsh
@@ -63,6 +63,13 @@ echo "\n3.a) Part 2: Setting Up Subnet Router...\n"
 
 sudo tailscale set --advertise-routes=192.168.0.0/24
 
+echo "\n3.b) Linux optimizations for subnet routers and exit nodes...\n"
+printf '#!/bin/sh\n\nethtool -K %s rx-udp-gro-forwarding on rx-gro-list off \n' "$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")" | sudo tee /etc/networkd-dispatcher/routable.d/50-tailscale
+sudo chmod 755 /etc/networkd-dispatcher/routable.d/50-tailscale
+
+sudo /etc/networkd-dispatcher/routable.d/50-tailscale
+test $? -eq 0 || echo 'An error occurred.'
+
 #----------------------------------------------------------------------
 # Rust Setup
 #----------------------------------------------------------------------
